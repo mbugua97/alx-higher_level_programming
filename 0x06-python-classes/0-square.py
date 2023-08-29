3a6858b258c474b5d6ee821f58c5cda23282afc7
nganga mbugua <jobugua97@gmail.com>
2023-08-29 10:31:26 +0300

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square Class
+
+Square class that does nothing
+
+"""
+
+
+class Square:
+    """Empty square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Square representation"""
+
+    def __init__(self, size):
+        """__init__
+
+        __init__ initializes the square size value
+
+        Args: size (int): Square size
+
+        """
+        self.__size = size
